ff40bf789cbef9adc97676c8d1b478b655578330
Marcus Wieder <31651017+wiederm@users.noreply.github.com>
2024-06-13 08:54:03 +0300

    chore: Update train.sh script to support optional accelerator and device arguments

--- experiments/train_on_energies/train.sh
+++ experiments/train_on_energies/train.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Parse command-line arguments for optional accelerator and device
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --accelerator) accelerator="$2"; shift ;;
+        --device) device="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
 # Define arrays for potentials and datasets
 potentials=("ani2x" "painn" "sake" "schnet" "physnet")
@@ -11,10 +20,28 @@ for potential in "${potentials[@]}"; do
         # Repeat each training 3 times
         for run in {1..3}; do
             echo "Running training for potential: $potential, dataset: $dataset, run: $run"
-            python ../../scripts/perform_training.py \
-            --potential_path="../../configs/potential_${potential}.toml" \
-            --dataset_path="../../configs/dataset_${dataset}.toml" \
-            --training_path="../../configs/training.toml"
+            # Construct the python command
+            python_cmd="python perform_training.py ../../scripts/perform_training.py \
+            --potential_path=\"../../configs/potential_${potential}.toml\" \
+            --dataset_path=\"../../configs/dataset_${dataset}.toml\" \
+            --training_path=\"../../configs/training.toml\""
+
+            # Add optional accelerator and device arguments if provided
+            if [ -n "$accelerator" ]; then
+                python_cmd+=" --accelerator=\"$accelerator\""
+            fi
+            if [ -n "$device" ]; then
+                python_cmd+=" --device=\"$device\""
+            fi
+
+            # Execute the python command
+            echo $python_cmd
+            eval $python_cmd
+
+            
+            
+            python 
+
         done
     done
 done
